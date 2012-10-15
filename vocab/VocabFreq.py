8466d9c02631255b4f98bdadf0391de67d81c212
dirkchen <bodong.chen@gmail.com>
2012-10-15 00:14:30 -0400

    output csv

--- vocab/VocabFreq.py
+++ vocab/VocabFreq.py
@@ -1,38 +1,52 @@
-import csv, glob
+import csv, glob, shutil
 
+# Parse dictionary, store keys
 file_dic = open(glob.glob('*.txt')[0])
 dic = []
 for line in file_dic:
     new=line.lower().strip()
     dic.append(new)
 
-
+# Initialize csv reader
 file_csv = open(glob.glob('*.csv')[0], "r")
 csv.field_size_limit(10000000)
-reader_csv = csv.reader(file_csv, dialect='excel')
+reader_csv = csv.reader(file_csv, dialect = 'excel')
 word_count = {}
+
+# Compare each row to key in dictionary dic
 for row in reader_csv:
-    for i in dic:
+    
+    for i in dic: 
         new_row=row[2].lower().strip()
         
         if i in new_row:
             count = new_row.count(i)
+            
             if i not in word_count:
                 word_count[i] = count
             else:
-                word_count[i] = word_count[i]+count
-                
+                word_count[i] = word_count[i] + count
+# Close stream                
 file_dic.close()
 file_csv.close()
 
 
-write_to = open(glob.glob('*.csv')[0]+'.txt', 'w')
+# Copy original file
+shutil.copyfile(glob.glob('*.csv')[0], 'output.csv')
+# Overwrite copied file
+write_to = open('output.csv', 'w')
+# Initialize csv writer
+writer_csv = csv.writer(write_to, dialect = 'excel')
 
-word_keys = word_count.keys()
-word_keys.sort()
-for key in word_keys:
-    write_to.write(key.rjust(40)+' '+ str(word_count[key])+'\n')
+# Write to overwritten file
+for key in dic:
+
+    if key in word_count:
+        get_value = word_count[key]        
+        writer_csv.writerow([key, get_value])
     
+    else:        
+        writer_csv.writerow([key, 0])
+# Close stream      
 write_to.close()
 
-   
